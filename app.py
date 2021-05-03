d67276f80bfc25c3a64313080028c912e8da977d
Sudeep Sawant <sud0@live.com>
2021-05-03 22:01:44 +0100

    Second Commit2t

--- app.py
+++ app.py
@@ -66,5 +66,8 @@ def my_form_post():
         return render_template('home.html')
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+        import os
+        port = int(os.environ.get('PORT', 33507))
+        app.run(host='0.0.0.0', port=port)
+
